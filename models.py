db3d1ef3ae1e9da6e9236fc7bf94ea151178e09e
cooper <tokyolabs@protonmail.com>
2022-09-25 16:39:16 -0400

    wip

--- models.py
+++ models.py
@@ -12,7 +12,7 @@ class Role(str, Enum):
     female = "user"
 
 class User(BaseModel):
-    id:Optional[UUID] = uuid4
+    id:Optional[UUID] = uuid4()
     first_name: str
     last_name: str
     middle_name: Optional[str]
